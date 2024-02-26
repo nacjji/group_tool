72d50a446907f7a8bf4419b035bafaca2165e308
jiwon na <jiwon.na@fig.xyz>
2024-02-26 23:21:10 +0900

    initialize

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('GROUP TOOL')
+    .setDescription('소규모 그룹 툴 API docs')
+    .setVersion('1.0.0')
+    // .addBearerAuth(
+    //   {
+    //     type: 'http',
+    //     scheme: 'bearer',
+    //   },
+    //   'access-token',
+    // )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+  console.log(process.env.PORT);
+  await app.listen(8000);
 }
 bootstrap();
